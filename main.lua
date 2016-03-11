d704defee987a58183dab647b5b7199ef09d3b16
Kevin Harrison <keharriso@gmail.com>
2016-03-10 23:34:27 -0500

    add basic game skeleton and future dependencies

---
+++ main.lua
@@ -0,0 +1,18 @@
+-- Rogue with Friends - a 2D, real-time, multiplayer roguelike
+-- -----------------------------------------------------------
+-- Released under the GPLv3 or later. See README.md for info.
+
+-- This file is executed by the LÖVE engine upon starting the game. It
+-- performs initialization and registers love.* callbacks with the engine.
+
+function love.load(arg)
+	-- TODO
+end
+
+function love.update(dt)
+	-- TODO
+end
+
+function love.draw()
+	-- TODO
+end
