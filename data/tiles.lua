049abd25c60f3207262ff387e2040a08a4fb2348
Kevin Harrison <keharriso@gmail.com>
2016-03-20 02:09:37 -0400

    add tiles and entities for milestone 0

---
+++ data/tiles.lua
@@ -0,0 +1,11 @@
+-- Rogue with Friends - a 2D, real-time, multiplayer roguelike
+-- ---------------------------------------------------------------
+-- Released under the GNU AGPLv3 or later. See README.md for info.
+
+return {
+
+Floor = {solid = false};
+
+Wall = {solid = true};
+
+}
